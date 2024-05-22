1305ed7959e56ffe3564e495a56371d891bf221c
xuanhieu12345 <hieuxuan213@gmail.com>
2024-05-22 14:53:11 +0700

    init project

---
+++ src/hoidanit/HelloWorld.java
@@ -0,0 +1,12 @@
+package hoidanit;
+
+public final class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world with java and hoi dan IT");
+		System.out.println("hello world with java and hoi dan IT");
+		System.out.println("hello world with java and hoi dan IT");
+		System.out.println("hello world with java and hoi dan IT");
+		System.out.println("hello world with java and hoi dan IT");
+		System.out.println("hello world with java and hoi dan IT");
+	}
+}
